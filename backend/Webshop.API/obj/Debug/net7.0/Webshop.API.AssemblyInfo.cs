7d816f1d57372c9bca35452802163efd814749f3
AbdelhafezMohammed <mohammed.abd.elhady@outlook.com>
2024-02-13 13:42:08 +0100

    add gitignore file

--- backend/Webshop.API/obj/Debug/net7.0/Webshop.API.AssemblyInfo.cs
+++ backend/Webshop.API/obj/Debug/net7.0/Webshop.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webshop.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4295a093346884ee730f173484e7182c6eb6d83c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webshop.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webshop.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
